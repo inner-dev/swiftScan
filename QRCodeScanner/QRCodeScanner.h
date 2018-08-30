0629d3b5a1bae1c8d004806476673d41b5b65a8b
zfmai <waltcow@gmail.com>
2018-08-30 10:15:02 +0800

    add carthage support

---
+++ QRCodeScanner/QRCodeScanner.h
@@ -0,0 +1,19 @@
+//
+//  QRCodeScanner.h
+//  QRCodeScanner
+//
+//  Created by maizifeng on 2018/8/30.
+//  Copyright © 2018 xialibing. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for QRCodeScanner.
+FOUNDATION_EXPORT double QRCodeScannerVersionNumber;
+
+//! Project version string for QRCodeScanner.
+FOUNDATION_EXPORT const unsigned char QRCodeScannerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <QRCodeScanner/PublicHeader.h>
+
+
